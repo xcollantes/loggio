ef2d7225a73a3b0238781b3a919d0e4cb45a3003
Xavier <collantes.xavier@gmail.com>
2025-12-13 18:03:33 -0800

    Add __init__ import for PyPi
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- __init__.py
+++ __init__.py
@@ -2,3 +2,13 @@
 
 This module provides easy access to the enhanced logging utilities.
 """
+
+from .enhanced_logger import (
+    ColoredFormatter,
+    Colors,
+    EnhancedLogger,
+    get_logger,
+)
+
+__version__ = "0.1.0"
+__all__ = ["Colors", "ColoredFormatter", "EnhancedLogger", "get_logger"]
